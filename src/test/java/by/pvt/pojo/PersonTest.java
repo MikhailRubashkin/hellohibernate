977eaa18c9fd3013c5388e564107211e22fbdfdf
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-07-27 14:07:09 +0300

    creat+

--- src/test/java/by/pvt/pojo/PersonTest.java
+++ src/test/java/by/pvt/pojo/PersonTest.java
@@ -3,11 +3,9 @@ package by.pvt.pojo;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.mapping.MetadataSource;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class PersonTest {
     @Test
