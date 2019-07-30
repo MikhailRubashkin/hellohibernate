331e2b7b3de9310d084de04fc00713877ae8c2af
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-07-30 11:15:40 +0300

    error correction + test

--- src/test/java/by/pvt/pojo/PersonTest.java
+++ src/test/java/by/pvt/pojo/PersonTest.java
@@ -1,23 +1,65 @@
 package by.pvt.pojo;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Date;
+import java.util.List;
+
+import static by.pvt.util.HibernateUtil.getInstance;
+import static org.junit.Assert.assertEquals;
+
 
 public class PersonTest {
-    @Test
-    public void testPerson(){
 
-        SessionFactory sessionFactory =
-                new MetadataSources (
-                        new StandardServiceRegistryBuilder ()
-                .configure()
-                .build()
-                ).buildMetadata().buildSessionFactory();
-        assertNotNull ( sessionFactory );
+    private static Person createTestDate (){
+        Person person = new Person ( );
+        person.setId (1);
+        person.setFirstName ("FirstName");
+        person.setLastName ("LastName");
+        person.setDateOfBirth (new Date ( ));
+        person.setGender ('a');
+        return person;
     }
 
+
+    @Test
+    public void testSavePerson (){
+        Session session = getInstance ( ).getSession ( );
+        Transaction tx = null;
+
+        try {
+
+            tx = session.beginTransaction ( );
+            Person p = createTestDate ( );
+            session.save (p);
+            tx.commit ( );
+            session.close ( );
+
+
+            session = getInstance ( ).getSession ( );
+            tx = session.beginTransaction ( );
+            List<Person> personList =
+                    session.createQuery ("from person").list ( );
+
+            assertEquals (1, personList.size ( ));
+            Person p2 = personList.get (0);
+            assertEquals (p, p2);
+            tx.commit ( );
+            session.close ( );
+
+            session = getInstance ( ).getSession ( );
+            tx = session.beginTransaction ( );
+            Person loadedPerson = session.load (Person.class, 1);
+            assertEquals (p, loadedPerson);
+            tx.commit ( );
+
+        } catch (Exception e) {
+            if (tx != null) tx.rollback ( );
+            throw e;
+        } finally {
+            session.close ( );
+        }
+    }
 }
